d12a7998caf680f884c758d70f82c5ee4bf2b8d9
Joas Scicchitano <joasscicchitano@MacBook-di-Joas.local>
2014-11-25 10:56:19 +0100

    Initial Commit

---
+++ hyscoswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hyscoswift
+//
+//  Created by Joas Scicchitano on 25/11/14.
+//  Copyright (c) 2014 ___informaticamilano___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
